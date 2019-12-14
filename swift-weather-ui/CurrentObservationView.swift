ed40c78a22cc3535afcfb38cce463cdf7d5de4cc
Guven Bolukbasi <guvenbolukbasi@gmail.com>
2019-12-14 15:20:32 +0300

    applied the dark mode fix

--- swift-weather-ui/CurrentObservationView.swift
+++ swift-weather-ui/CurrentObservationView.swift
@@ -43,13 +43,14 @@ struct CurrentObservationView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
 		Group {
-			
+
 			// Regular
 			CurrentObservationView()
 
 			// Dark
 			CurrentObservationView()
 				.environment(\.colorScheme, .dark)
+				.darkModeFix()
 
 			// XS
 			CurrentObservationView()
@@ -59,5 +60,37 @@ struct ContentView_Previews: PreviewProvider {
 			CurrentObservationView()
 				.environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
 		}
+    }
+}
+
+
+public struct DarkView<Content> : View where Content : View {
+    var darkContent: Content
+    var on: Bool
+    public init(_ on: Bool, @ViewBuilder content: () -> Content) {
+        self.darkContent = content()
+        self.on = on
+    }
+
+    public var body: some View {
+        ZStack {
+            if on {
+                Spacer()
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(Color.black)
+                    .edgesIgnoringSafeArea(.all)
+                darkContent.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity).background(Color.black).colorScheme(.dark)
+            } else {
+                darkContent
+            }
+        }
+    }
+}
+
+extension View {
+    public func darkModeFix(_ on: Bool = true) -> DarkView<Self> {
+        DarkView(on) {
+            self
+        }
     }
 }
