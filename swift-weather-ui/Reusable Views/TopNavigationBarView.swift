e5856ce1bff8bfca3194b43cc0911059d8027948
Guven Bolukbasi <guvenbolukbasi@gmail.com>
2019-12-21 13:14:50 +0300

    started building the top navigation bar view

---
+++ swift-weather-ui/Reusable Views/TopNavigationBarView.swift
@@ -0,0 +1,43 @@
+//
+//  TopNavigationBarView.swift
+//  swift-weather-ui
+//
+//  Created by Guven Bolukbasi on 21.12.2019.
+//  Copyright © 2019 dorianlabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopNavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopNavigationBarView_Previews: PreviewProvider {
+
+    static var previews: some View {
+		Group {
+
+			// Regular
+			TopNavigationBarView()
+
+			// Regular on SE
+			TopNavigationBarView()
+				.previewDevice("iPhone SE")
+
+			// Dark
+			TopNavigationBarView()
+				.environment(\.colorScheme, .dark)
+				.darkModeFix()
+
+			// XS
+			TopNavigationBarView()
+				.environment(\.sizeCategory, .extraSmall)
+
+			// XXL
+			TopNavigationBarView()
+				.environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+		}
+    }
+}
