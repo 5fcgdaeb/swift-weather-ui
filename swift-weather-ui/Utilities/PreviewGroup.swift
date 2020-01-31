3d889622db0b1d90db3fc8d16bdbeb602e2d6be3
Guven Bolukbasi <guvenbolukbasi@gmail.com>
2020-01-31 22:02:11 +0100

    an effort to refactor all the preview provider code

---
+++ swift-weather-ui/Utilities/PreviewGroup.swift
@@ -0,0 +1,48 @@
+//
+//  PreviewGroup.swift
+//  swift-weather-ui
+//
+//  Created by Guven Bolukbasi on 31.01.2020.
+//  Copyright © 2020 dorianlabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct PreviewGroupProvider {
+
+//    static func theGroup(theViewUnderPreview: some View) -> some View {
+//        return Group {
+//
+//            // Pure View
+//            theViewUnderPreview
+//                .previewDisplayName("Pure")
+//                .previewLayout(.sizeThatFits)
+//            
+//            // Regular
+//            theViewUnderPreview
+//                .previewDisplayName("Simple")
+//
+//            // Regular on SE
+//            theViewUnderPreview
+//                .previewDevice("iPhone SE")
+//                .previewDisplayName("iPhone SE")
+//
+//            // Dark
+//            theViewUnderPreview
+//                .environment(\.colorScheme, .dark)
+//                .darkModeFix()
+//                .previewDisplayName("Dark mode")
+//
+//            // XS
+//            theViewUnderPreview
+//                .environment(\.sizeCategory, .extraSmall)
+//                .previewDisplayName("XS Fonts")
+//
+//            // XXL
+//            theViewUnderPreview
+//                .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+//                .previewDisplayName("XXXXL Fonts")
+//        }
+//    }
+    
+}
